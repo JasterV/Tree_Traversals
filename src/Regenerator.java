2e756686310d56a877d3577823f5b4a814debdb1
jasterv <victorcoder2@gmail.com>
2019-12-18 11:36:18 +0100

    some spaghetti names changed

--- src/Regenerator.java
+++ src/Regenerator.java
@@ -3,22 +3,22 @@ import java.util.List;
 public class Regenerator {
     public static <E> LinkedBinaryTree<E> postAndIn(List<? extends E> postOrder, List<? extends E> inOrder) {
         int treeSize = postOrder.size();
-        if (treeSize == 0) {
+        if (treeSize == 0)
             return new LinkedBinaryTree<>();
-        } else if ( treeSize == 1){
+        else if (treeSize == 1) {
             E root = postOrder.get(treeSize - 1);
-            return new  LinkedBinaryTree<>(root);
-        }else {
+            return new LinkedBinaryTree<>(root);
+        } else {
             E root = postOrder.get(treeSize - 1);
             int indexOfRoot = inOrder.indexOf(root);
-            List<? extends E> subInLeft =  inOrder.subList(0, indexOfRoot);
-            List<? extends E> subInRight =  inOrder.subList(indexOfRoot + 1, treeSize);
-            List<? extends E> subPosLeft = postOrder.subList(0, indexOfRoot);
-            List<? extends E> subPosRight = postOrder.subList(indexOfRoot, treeSize - 1);
-         
+            List<? extends E> inOrderLeft = inOrder.subList(0, indexOfRoot);
+            List<? extends E> inOrderRight = inOrder.subList(indexOfRoot + 1, treeSize);
+            List<? extends E> postOrderLeft = postOrder.subList(0, indexOfRoot);
+            List<? extends E> postOrderRight = postOrder.subList(indexOfRoot, treeSize - 1);
+
             return new LinkedBinaryTree<>(root,
-                    postAndIn(subPosLeft,subInLeft),
-                    postAndIn(subPosRight,subInRight));
+                    postAndIn(postOrderLeft, inOrderLeft),
+                    postAndIn(postOrderRight, inOrderRight));
         }
     }
 }
