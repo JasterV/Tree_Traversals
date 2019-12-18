35ef38dce85157c48a9e5038e2298c262269b381
Víctor Martínez Montané <victorcoder2@gmail.com>
2019-12-18 11:28:39 +0100

    spaghetti

--- src/Regenerator.java
+++ src/Regenerator.java
@@ -3,18 +3,14 @@ import java.util.List;
 public class Regenerator {
     public static <E> LinkedBinaryTree<E> postAndIn(List<? extends E> postOrder, List<? extends E> inOrder) {
         int treeSize = postOrder.size();
-        if (treeSize == 0) {
+        if (treeSize == 0)
             return null;
-        } else {
+        else {
             E root = postOrder.get(treeSize - 1);
             int indexOfRoot = inOrder.indexOf(root);
             return new LinkedBinaryTree<>(root,
-                    postAndIn(postOrder.subList(0, indexOfRoot),
-                            inOrder.subList(0, indexOfRoot)
-                    ),
-                    postAndIn(postOrder.subList(indexOfRoot, treeSize - 1),
-                            inOrder.subList(indexOfRoot + 1, treeSize)
-                    )
+                    postAndIn(postOrder.subList(0, indexOfRoot), inOrder.subList(0, indexOfRoot)),
+                    postAndIn(postOrder.subList(indexOfRoot, treeSize - 1), inOrder.subList(indexOfRoot + 1, treeSize))
             );
         }
     }
